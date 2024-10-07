9539da8c4864578132b792e6807e5d9cba5e6400
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-10-07 17:01:03 +0530

    Added

--- clientside/js/index.js
+++ clientside/js/index.js
@@ -3,8 +3,11 @@ async function getProducts() {
 
       const res=await fetch("http://localhost:3000/api/getproducts",{headers:{
         "Authorization" : `Bearer ${value}`}})
+        const result = await res.json();
     if(res.status==200){
-        alert("hi")
+            document.getElementById("profileImage").src=`${result.profile}`
+            document.getElementById("img2").src=`${result.profile}`
+            document.getElementById("link").innerHTML=`<a href="./pages/profile.html?id=${result.id}"><button>View or Edit Profile</button></a>`
     }
     else{
         alert("Error");
@@ -12,18 +15,21 @@ async function getProducts() {
     }
 }
 getProducts();
-function profile() {
-    document.getElementById("dropdown").innerHTML=`
-    <div class="up">></div>
-    <div class="down">
-                <div class="image">
-                    <img src="" alt="">
-                </div>
-                <div class="buttons">
-                    <h3>Name</h3>
-                    <button>View Profile</button>
-                    <button>Logout</button>
-                </div>
-            </div>
-    `;
+const profileImage = document.getElementById('profileImage');
+const dropdownMenu = document.getElementById('dropdownMenu');
+
+function dropdown(){
+    dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+}
+
+// Close dropdown if clicked outside
+window.addEventListener('click', (event) => {
+    if (!profileImage.contains(event.target) && !dropdownMenu.contains(event.target)) {
+        dropdownMenu.style.display = 'none';
+    }
+});
+
+function logout(){
+    localStorage.removeItem("Auth");
+    window.location.href="../pages/signin.html"
 }
--- serverside/requestHandler.js
+++ serverside/requestHandler.js
@@ -12,8 +12,8 @@ export async function getProducts(req,res) {
         console.log(user);
         if(!user) 
             return res.status(403).send({msg:"Unauthorized access"})
-        const employees=await userSchema.find();
-        res.status(200).send({employees,username:user.username})
+        const products=await userSchema.find();
+        res.status(200).send({products,profile:user.profile,id:user._id})
         
     } catch (error) {
         res.status(404).send({msg:error})
@@ -21,11 +21,23 @@ export async function getProducts(req,res) {
 }
 
 
+export async function getUser(req,res) {
+    try {
+        const {id}=req.params;
+        const data=await userSchema.findOne({_id:id});
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(404).send(error)
+    }
+}
+
+
+
 export async function signUp(req,res) {
     try{
-        const {email,username,password,cpassword } = req.body;
-        console.log(email,username,password,cpassword);
-        if(!(email&& username&& password&& cpassword))
+        const {email,username,password,cpassword,place,profile,address,phone,pincode} = req.body;
+        console.log(email,username,password,cpassword,place,profile,address,phone,pincode);
+        if(!(email&& username&& password&& cpassword && place && profile && address && phone && pincode))
             return res.status(404).send({msg:"fields are empty"})
         if(password !== cpassword)
             return res.status(404).send({msg:"password not matching"})
@@ -34,7 +46,7 @@ export async function signUp(req,res) {
         .then((hashedPassword)=>{
             console.log(hashedPassword);
             userSchema
-            .create({email,username,password:hashedPassword})
+            .create({email,username,password:hashedPassword,place,profile,address,phone,pincode})
             .then(()=>{
                 console.log("success");
                 return res.status(201).send({msg:"successs"})
